93e80bea7cc2413b0b53288f069323d69d8002b0
namanmalhotra26 <namanmalhotra267@gmail.com>
2022-06-16 19:21:14 +0530

    initail commit

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ module.exports.isloggedin = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be loged in');
-        return res.redirect('/login')
+        return res.redirect('/')
     }
     next();
 }
